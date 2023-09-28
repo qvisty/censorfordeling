897ebdcaff92208583941eeba1ee4c07cf438bca
Jesper Qvist <jgq@live.dk>
2023-09-28 13:39:00 +0200

    pause

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,13 +46,21 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+# DATABASES = {
+#     "default": env.db(
+#         "DATABASE_URL",
+#         default="postgres://localhost/censorfordeling",
+#     ),
+# }
+# DATABASES["default"]["ATOMIC_REQUESTS"] = True
 DATABASES = {
-    "default": env.db(
-        "DATABASE_URL",
-        default="postgres://localhost/censorfordeling",
-    ),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-DATABASES["default"]["ATOMIC_REQUESTS"] = True
+
+
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
